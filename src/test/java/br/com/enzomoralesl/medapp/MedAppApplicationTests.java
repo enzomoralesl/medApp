3148fcfff29d62304ca6d708429ab8e86aa82857
enzomoralesl <enzo_moralesl@yahoo.com.br>
2024-11-26 22:34:57 -0300

    initial structure

---
+++ src/test/java/br/com/enzomoralesl/medapp/MedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.enzomoralesl.medapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedAppApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
