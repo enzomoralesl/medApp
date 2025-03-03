2fa714686a0be1db857d9330d6816a8a93a45cd0
enzomoralesl <enzo_moralesl@yahoo.com.br>
2025-03-03 17:31:28 -0300

    feat: database script init/drop

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,5 @@
--- Enable the uuid-ossp extension
-SET search_path TO public;
-DROP EXTENSION IF EXISTS "uuid-ossp";
-
-CREATE EXTENSION "uuid-ossp" SCHEMA public;
-
-DROP TABLE IF EXISTS tb_surgery;
-DROP TABLE IF EXISTS tb_doctor;
-DROP TABLE IF EXISTS tb_medical_record;
-DROP TABLE IF EXISTS tb_patient;
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
 
 -- Create tb_patient table
 CREATE TABLE IF NOT EXISTS tb_patient (
@@ -44,26 +36,3 @@ CREATE TABLE IF NOT EXISTS tb_surgery (
                                           type VARCHAR(50),
                                           medical_record_id UUID REFERENCES tb_medical_record(id)
 );
-
--- Insert data into tb_patient
-INSERT INTO tb_patient (id, email, name, cpf, password, phone, birth_date)
-VALUES
-    (uuid_generate_v4(), 'john1000.doee@example.com', 'John doee', '12345678901', 'password', '1234567890', '1990-01-01'),
-    (uuid_generate_v4(), 'enzo.moralesl@example.com', 'Enzo Morales', '12312312345', 'password', '1234567891', '1992-02-02');
-
--- Insert data into tb_medical_record
-INSERT INTO tb_medical_record (id, patient_id)
-VALUES
-    (uuid_generate_v4(), (SELECT id FROM tb_patient WHERE email = 'john1000.doee@example.com')),
-    (uuid_generate_v4(), (SELECT id FROM tb_patient WHERE email = 'enzo.moralesl@example.com'));
-
--- Insert data into tb_doctor
-INSERT INTO tb_doctor (id, name, specialty, crm)
-VALUES
-    (uuid_generate_v4(), 'Dr. Smith', 'Cardiology', 'DESCUBRAA');
-
--- Insert data into tb_surgery
-INSERT INTO tb_surgery (id, date, doctor_name, doctorcrm, description, type, medical_record_id)
-VALUES
-    (uuid_generate_v4(), '2025-03-01', 'Dr. Smith', 'DESCUBRAA', 'Heart surgery', 'COMPLEX', (SELECT id FROM tb_medical_record WHERE patient_id = (SELECT id FROM tb_patient WHERE email = 'john1000.doee@example.com'))),
-    (uuid_generate_v4(), '2025-04-01', 'Dr. Smith', 'DESCUBRAA', 'Knee surgery', 'COMPLEX', (SELECT id FROM tb_medical_record WHERE patient_id = (SELECT id FROM tb_patient WHERE email = 'enzo.moralesl@example.com')));
