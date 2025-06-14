bd1210c983d3481a68bb0146329cdf80d6ee47e7
enzomoralesl <enzo_moralesl@yahoo.com.br>
2025-06-14 00:13:47 -0300

    fix: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 export PGPASSWORD=12345
 
 # Aguarda o banco de dados estar disponível
-until pg_isready -h db -p 5432 -U user; do
+until pg_isready -h db -p 5432 -U postgres; do
   echo "Aguardando o banco de dados..."
   sleep 2
 done
@@ -12,7 +12,7 @@ done
 # Verifica se o arquivo schema.sql existe antes de tentar executá-lo
 if [ -f /app/schema.sql ]; then
   echo "Executando o schema.sql..."
-  psql -h db -U user -d postgres -f /app/schema.sql
+  psql -h db -U postgres -d postgres -f /app/schema.sql
 else
   echo "Arquivo schema.sql não encontrado!"
 fi
